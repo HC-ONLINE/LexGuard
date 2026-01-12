7ede7a3b8a2d10dcf702bd9af3ae6257d250b9ea
HC-ONLINE <henriquezandres856@gmail.com>
2026-01-12 11:00:32 -0500

    feat: agrega archivos de datos de tarjetas de prueba en formatos CSV, JSON, LOG y SQL

---
+++ data/test/test_cards.sql
@@ -0,0 +1,10 @@
+-- SQL DUMP DE PAGOS
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Juan', 'credit card number: 4532015112830366 exp 12/25', '4532015112830366');
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Ana', 'card=5555555555554444', '5555555555554444');
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Pedro', 'Customer VISA 4532015112830366 approved', '4532015112830366');
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Maria', 'credit card number: 4532015112830366', '4532015112830366');
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Luis', 'card 5555555555554444 cvv 123', '5555555555554444');
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Sofia', 'Possible card: 4532015112830366', '4532015112830366');
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Carlos', 'Backup payment number 5555555555554444', '5555555555554444');
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Elena', '4532-0151-1283-0366', '4532-0151-1283-0366');
+INSERT INTO pagos (cliente, comentario, tarjeta) VALUES ('Rosa', '4532015112830366', '4532015112830366');
