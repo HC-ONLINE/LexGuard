0eb1138f3e9d1c1d1968ff9de826e0a4d381321f
HC-ONLINE <henriquezandres856@gmail.com>
2026-03-05 15:23:56 -0500

    feat: añade archivos de prueba para NIT/RUT colombiano en formatos CSV, JSON, SQL y TXT

---
+++ data/nit_co/test/test_nit.sql
@@ -0,0 +1,7 @@
+-- SQL DUMP DE PRUEBAS DE NIT/RUT COLOMBIANO
+INSERT INTO empresas (razon_social, comentario, nit) VALUES ('Empresa ABC', 'NIT 800.197.268-4', '800197268-4');
+INSERT INTO empresas (razon_social, comentario, nit) VALUES ('Constructora XYZ', 'RUT: 901.234.567-7', '901234567-7');
+INSERT INTO empresas (razon_social, comentario, nit) VALUES ('Logistica SA', 'nit 900.456.789-4', '900456789-4');
+INSERT INTO empresas (razon_social, comentario, nit) VALUES ('Comercial LTD', 'NIT: 700.456.789-9', '700456789-9');
+INSERT INTO empresas (razon_social, comentario, nit) VALUES ('Invalido', 'nit 123456789-0', '123456789-0');
+INSERT INTO empresas (razon_social, comentario, nit) VALUES ('NoMatch', 'numero 987654321-1', '987654321-1');
